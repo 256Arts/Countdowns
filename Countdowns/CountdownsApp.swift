dc03cb221f6c0dd37e2938c4e1cb43b11b76f828
Jayden Irwin <me@jaydenirwin.com>
2022-07-30 09:21:10 -0400

    Initial Commit

---
+++ Countdowns/CountdownsApp.swift
@@ -0,0 +1,17 @@
+//
+//  CountdownsApp.swift
+//  Countdowns
+//
+//  Created by Jayden Irwin on 2022-07-30.
+//
+
+import SwiftUI
+
+@main
+struct CountdownsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
